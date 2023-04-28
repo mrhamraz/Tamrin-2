e3485213714d7f44992d2cbf88def118fd53889c
Hamraz <x6.reza@gmail.com>
2023-04-28 20:39:05 +0430

    Add project files.

---
+++ 1. Check Equality/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1._Check_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
