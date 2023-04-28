d76434cc7b95b489acff02434d80c59bab9f2944
Hamraz <x6.reza@gmail.com>
2023-04-28 21:35:34 +0430

    2.2

---
+++ 2. Check Even or Odd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2._Check_Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to check if it's even or odd :");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+
+                Console.WriteLine("Even");
+
+            else
+                Console.WriteLine("Odd");
+        }
+    }
+}
