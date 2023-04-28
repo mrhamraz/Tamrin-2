d9fb750b689485a41419381f305dc32d010af868
Hamraz <x6.reza@gmail.com>
2023-04-28 21:58:41 +0430

    2.3

---
+++ 3. Check Positive or Negative/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _3._Check_Positive_or_Negative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to check the sign of it  :");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 0)
+
+                Console.WriteLine("Positive");
+
+            else
+                Console.WriteLine("Negative");
+        }
+    }
+}
