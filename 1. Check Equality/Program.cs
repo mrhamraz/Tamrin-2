d279bc96c2da69e9eca28a95eedf97b18b85d09b
Hamraz <x6.reza@gmail.com>
2023-04-28 21:04:58 +0430

    TEST

--- 1. Check Equality/Program.cs
+++ 1. Check Equality/Program.cs
@@ -6,7 +6,18 @@ namespace _1._Check_Equality
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter to numbers to check the equality : ");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2)
             
+                Console.WriteLine("EQUAL");
+
+            else
+                Console.WriteLine("NOT EQUAL");
+
         }
     }
 }
