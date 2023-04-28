91b93ba7a901346fb70473bf4971609c2b5e8aa8
Hamraz <x6.reza@gmail.com>
2023-04-28 23:33:13 +0430

    2.5

---
+++ 5. 1 0 -1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5._1_0__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number :");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 0)
+                Console.WriteLine("0");
+            else if (num > 0)
+                Console.WriteLine("1");
+            else if (num < 0)
+                Console.WriteLine("-1");
+        }
+    }
+}
