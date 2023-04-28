27a3755e9566de557e0e0bc2e71bf26519c3b81a
Hamraz <x6.reza@gmail.com>
2023-04-28 23:54:36 +0430

    2.6

---
+++ 6. Height/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _6._Height
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your heigth :");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0 && num < 150)
+                Console.WriteLine("Short");
+
+            else if (num > 150 && num < 165)
+                Console.WriteLine("Average");
+
+            else if (num > 165 && num < 195)
+                Console.WriteLine("Tall");
+
+            else if (num > 190)
+                Console.WriteLine("Too tall");
+
+            else
+                Console.WriteLine("unknown");
+
+        }
+    } 
+}
