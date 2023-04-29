7007c6feb396c642108e41e55f9cd8cd7e5209f4
Hamraz <x6.reza@gmail.com>
2023-04-29 15:29:11 +0430

    2.11

---
+++ 11. Vowel (if)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _11._Vowel__if_
+{
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter a character to check if it's vowel :");
+
+                char Char = Convert.ToChar(Console.ReadLine());
+
+                Char = Char.ToLower(Char);
+
+                char[] vowel = { 'a','e','i','o','u' };
+
+
+                //using System.Linq for [].Contains
+                if (vowel.Contains(Char))
+                    // if ((new[] { 'a','e','i','o','u'}).Contains(Char))
+
+                    Console.WriteLine("Vowel");
+
+                else
+                    Console.WriteLine("Not Vowel");
+            }
+        }
+    
+}
+    
+
