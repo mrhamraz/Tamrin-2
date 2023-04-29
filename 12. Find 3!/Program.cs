834288c6cefcbf2cd0fdf89aef0a78f2ec461f95
Hamraz <x6.reza@gmail.com>
2023-04-29 15:32:08 +0430

    2.12

---
+++ 12. Find 3!/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _12._Find_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter num :");
+
+            string num = Console.ReadLine();
+
+            if ((num[num.Length - 1]) == '3')
+                Console.WriteLine("Find");
+            else
+                Console.WriteLine("no");
+        }
+    }
+}
