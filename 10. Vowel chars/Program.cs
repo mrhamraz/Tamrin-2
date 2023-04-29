d4e6ed50f0d55557c73f3d8d671823583f8bbe81
Hamraz <x6.reza@gmail.com>
2023-04-29 15:18:10 +0430

    2.10

---
+++ 10. Vowel chars/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _10._Vowel_chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character to check if it's vowel :");
+
+            char Char = Convert.ToChar(Console.ReadLine());
+
+            Char = Char.ToLower(Char);
+
+            switch (Char)
+            {
+                case 'a':
+                    Console.WriteLine("Vowel!");
+                    break;
+                case 'e':
+                    Console.WriteLine("Vowel!");
+                    break;
+                case 'i':
+                    Console.WriteLine("Vowel!");
+                    break;
+                case 'o':
+                    Console.WriteLine("Vowel!");
+                    break;
+                case 'u':
+                    Console.WriteLine("Vowel!");
+                    break;
+                default:
+                    Console.WriteLine("Not Vowel!");
+                    break;
+            }
+        }
+    }
+}
